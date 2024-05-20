4ba46c0b892b2a14d076202a4d0a8dcac494c3ac
tarunclub <tarunkm__@outlook.com>
2024-05-20 20:52:39 +0530

    fund raiser test init

---
+++ fund-raiser-dapp/test/FundMeTest.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    uint256 public number = 1;
+
+    function setUp() external {
+        number = 2;
+    }
+
+    function testDemo() internal view {
+        console.log(number);
+        assertEq(number, 2);
+    }
+}
