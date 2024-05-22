f43a445ee6747c89d24b28af7cabb6d6da5448e8
tarunclub <tarunkm__@outlook.com>
2024-05-22 17:09:36 +0530

    added constant, immutable and custom error

--- fund-raiser-dapp/src/FundMe.sol
+++ fund-raiser-dapp/src/FundMe.sol
@@ -2,31 +2,33 @@
 pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
     
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
 
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdraw() external onlyOwner {
-        require(msg.sender == owner, "must be owner");
+        require(msg.sender == i_owner, "must be owner");
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -37,8 +39,12 @@ contract FundMe {
         payable(msg.sender).transfer(address(this).balance);
     }
 
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
     modifier onlyOwner() {
-        require(msg.sender == owner, "sender must be owner");
+        require(msg.sender == i_owner, "sender must be owner");
         _;
     }
 }
--- fund-raiser-dapp/test/FundMeTest.t.sol
+++ fund-raiser-dapp/test/FundMeTest.t.sol
@@ -2,16 +2,27 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    uint256 public number = 1;
+    FundMe fundMe;
 
     function setUp() external {
-        number = 2;
+        fundMe = new FundMe();
     }
 
-    function testDemo() internal view {
-        console.log(number);
-        assertEq(number, 2);
+    function testMinimumDollarIsFive() external view { 
+        assertEq(fundMe.minimumUsd(), 5e18);
+    }
+    function testOwnerIsDeployer() external view {
+        console.log("owner: ", fundMe.owner());
+        console.log("this: ", address(this));
+        assertEq(fundMe.owner(), address(this));
+    }
+
+    function testPriceFeedVersion() external view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+        console.log("version: ", version);
     }
 }
